5f0b5b5edac5839a435e0216dad3efc63ad1075c
jabulane <jabumokoena85@gmail.com>
2023-04-22 09:24:59 -0700

    adding _printf.c _putchar.c and _puts.c

---
+++ _printf.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _printf - a function that produces output according to a format.
+ * Return: the number of characters printed
+ * @format: is a character string.
+ */ 
+
+int _printf(const char *format, ...)
+{
+	int index, char_count = 0;
+	va_list args;
+	va_start(args, format);
+
+	/*checks if pointers points to NULL*/
+	if (format == NULL)
+	{
+		return (-1);
+	}
+
+	/*loop of the arguments*/
+	for (index = 0; format[index]; index++)
+	{
+		/*checks if the current character is a format specifier %*/
+		if (format[index] == '%')
+		{
+			index++;
+			if (format[index] == 'c')
+			{
+				char_count += _putchar(va_arg(args, int));
+			}
+			else if (format[index] == 's')
+			{
+				char_count += _puts(va_arg(args, char *));
+			}
+			else if (format[index] == '%')
+			{
+				char_count += _putchar('%');
+			}
+		}
+		else
+		{
+			char_count += _putchar(format[index]);
+		}
+	}
+	va_end(args);
+	return (char_count);
+}
---
+++ _puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints string
+ * Return: it returns the str to print.
+ * @str: the str to print
+ */
+
+int _puts(char *str)
+{
+	int index, counted_chars;
+	
+	/*loop over the str*/
+	for (index = 0; str[index]; index++)
+	{
+		/*call _putchar to print curr char and increment the count*/
+		counted_chars += _putchar(str[index]);
+	}
+
+	/*return count of characters printed*/
+	return (counted_chars);
+}
