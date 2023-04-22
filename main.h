4c310780189d747364ece325560726c804415c80
jabulane <jabumokoena85@gmail.com>
2023-04-22 01:22:24 -0700

    adding file main.h

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_FILE_H
+#define MAIN_FILE_H
+#include <stdarg.h>
+
+/**
+ * _printf - a function that produces output according to a format.
+ * @format: is a character string.
+ */
+
+/*list of function prototypes*/
+int _printf(const char *format, ...);
+#endif
