da8b362f7562bc6e685f0f9d31c4de12fb32714a
Musa <musaqwabe@gmail.com>
2023-04-23 15:38:24 -0700

    Added prototypes in header file

--- main.h
+++ main.h
@@ -14,4 +14,17 @@
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _puts(char *str);
+void print_binary(unsigned int x);
+int print_binary_custom(const char *format, unsigned int x);
+void handle_length_modifiers(char modifier, char conversion_specifier, ...);
+void _flag_characters(int num, char specifier, char flags[]);
+int get_precision(const char *format, int *i, va_list lst);
+int get_width(const char *format, int *i, va_list lst);
+void handle_custom_specifier_r(char *buffer, int *i, int buf_size, va_list lst);
+void handle_field_width(const char *format, int *i, va_list lst, char *width);
+void handle_zero_flag(char *buffer, int *i, int width);
+void handle_precision(const char *format, int *i, va_list lst, int *precision);
+void handle_minus_flag(char *buffer, int *i, int buf_size,
+                int width, int precision, int _isnegative);
+int _isdigit(int c);
 #endif
